b8b26dae0e72ff4c797ebbb78c3c1b4653896f19
FernanWil <fernandowilfre.13@gmail.com>
2025-06-06 19:13:59 -0500

    mejorando las tareas

--- src/app/task/features/task/task.component.ts
+++ src/app/task/features/task/task.component.ts
@@ -1,58 +1,79 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { take } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-task',
-	imports: [CommonModule],
+	imports: [CommonModule, FormsModule],
 	templateUrl: './task.component.html',
 	styleUrl: './task.component.css'
 })
 export class TaskComponent {
 	@ViewChild('inputTask') inputTask!: ElementRef;
 	showButton: boolean = true;
-	task: string[] = [];
+	task:{id: number, task: string} [] = [];
 	contTask: number = 0;
 	completeTask: boolean = false;
 	onButtonIndex: number | null = null;
 	inputVisible: boolean = false;
-
-	showInput(){
-		this.showButton = false;
-	}
-	saveTask(){
-		if (this.inputTask.nativeElement.value && this.inputTask.nativeElement.value != '') {
-			this.task.push(this.inputTask.nativeElement.value);
-			this.contTask+=1;
-			this.showButton = true;
-		}
-		console.log(this.task);
-	}
-	cancelTask(){
-		this.showButton = true;
-	}
-
-	buttonCheckTask() {
-		console.log("HOLA");
-		this.completeTask = ! this.completeTask
-	}
+	valueInputTask: string = '';
+	inputFocus: boolean = false;
 
 	onMouseEnter(index: number){ this.onButtonIndex = index; }
+
 	onMouseLeave(){ this.onButtonIndex = null; }
 
-	editTask(index: number){
-		if (index >= 0) {
-			this.inputVisible = true;
-			const newInput = this.inputTask?.nativeElement.focus();
-			setTimeout(() => {
-				this.task.splice(index, 1,newInput);
-			});
+	cancelTask(){ this.showButton = true; }
+
+	showInput(){ this.showButton = false; }
+	
+	buttonCheckTask() { this.completeTask = !this.completeTask }
+
+	saveTask(){
+		const inputValue = this.inputTask.nativeElement.value.trim();
+		if (inputValue) {
+			this.task.push({id: this.contTask, task: inputValue});
+			this.showButton = true;
+			this.inputTask.nativeElement.value = '';
+			this.contTask+=1;
 		}		
 	}
 
+	editTask(index: number){
+		this.inputTask?.nativeElement.focus();
+		try {
+			this.inputFocus = true;
+			this.inputVisible = true;
+			this.onButtonIndex = index;
+			this.valueInputTask = this.task[index].task;
+			console.log(this.task[index].task);
+		} catch (error) {console.log("Error: ", error); }
+	}
+
+	showNewValueInput(event: Event){
+		const value = event.target as HTMLInputElement;
+		this.valueInputTask = value.value;		
+	}
+	
+	saveTaskEdit(index: number){
+		const newValueInputTask = this.valueInputTask.trim();
+		console.log(this.valueInputTask);
+		if (newValueInputTask) {
+			this.task[index].task = newValueInputTask;
+			this.inputVisible = false;
+			this.inputFocus = false;
+			this.inputTask.nativeElement.value = '';
+		}
+	}
+
 	removeTask(id: number){
-		if (id) {
+		try {
 			this.task.splice(id,1);
+			this.contTask -=1
+			this.inputFocus = false;
+			this.inputVisible = false;
+		} catch (error) {
+			console.log(`Error al obtener el id: ${error}`);
 		}
 	}
 }
